0b8dfef0229f51563d3498236e45c9909f4ef77d
Basu0369 <appknowlogybd@gmail.com>
2022-12-25 22:06:43 +0600

    Movie-streaming-UI

--- lib/screens/movie_screen.dart
+++ lib/screens/movie_screen.dart
@@ -178,6 +178,7 @@ class MovieScreen extends StatelessWidget {
                       Icons.play_arrow_rounded,
                       size: 40,
                       color: Colors.white,
+
                     )),
               ),
             )
